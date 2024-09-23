d4cc29a3fdbfd77b6f063adda6fc9dc6aac26a73
gabrieldeoliveira1 <gabriel.oliveira00619628@sesisenaipr.org.br>
2024-09-23 13:55:08 -0300

    Comit
    Primeira Parte

---
+++ Aula_1_Aprendendo_HTML/dados.js
@@ -0,0 +1,7 @@
+let banco_de_dados = [
+    {
+        titulo: "Gabriel de Oliveira",
+        descricao: "Estudante proativo, 17 anos, cursando o terceiro ano do ensino médio, com uma sólida paixão por tecnologia e ênfase em programação em C, C#, SQL, JavaScript e HTML. Comprometido em adquirir conhecimentos avançados, busco oportunidades para aplicar minhas habilidades em projetos desafiadores na área de desenvolvimento de sistemas e engenharia de software. Determinado a contribuir positivamente para o campo da tecnologia.",
+        tags: ["HTML", "C#", "C", "JavaScript"]
+    }
+]
